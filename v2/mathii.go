6282cde31628b99c859c5aea7d1854be25da4995
AlvinMrema <sonalpha023@gmail.com>
2024-09-12 10:00:38 +0300

    added v2 of mathii to support Generics

---
+++ v2/mathii.go
@@ -0,0 +1,14 @@
+// Package mathii contains the solution to the exercises in chapter 10 of Learning Go, 2nd edition.
+package mathii
+
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
+// Add is a function that takes in two integer values and returns their sum.
+// It follows the rules specified by https://www.mathsisfun.com/numbers/addition.html
+func Add[T Number](x, y T) T {
+	return x + y
+}
